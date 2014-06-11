e4526cda5c859dfc1c763887abfa42f395579a1e
marcsAtSkyhunter <marcs@skyhunter.com>
2014-06-11 18:07:35 +0000

    prep plusMinus app for use in tutorial2

--- apps/plusMinus/server/main.js
+++ apps/plusMinus/server/main.js
@@ -1,8 +1,35 @@
 /*global console */
 
+/**
+ * PlusMinus creates a service with 3 interwoven mini-services, each
+ * mini-service a single object. The 3 objects allow you to increment a
+ * counter, decrement the counter, and read the counter.
+ * 
+ * For example, suppose you have Alice, Bob, and Carol 
+ * working together. Alice wants to see if there are more blue cars
+ * driving down the highway than red cars. She wants Bob to increment a
+ * a counter each time he sees a blue car, and she wants Carol to decrement
+ * the counter each time she sees a red car. If the counter>0 at any time,
+ * more blue cars have passed; if the counter<0, more red cars have
+ * passed.
+ * 
+ * So Alice creates a plus/minus
+ * service with 3 webkeys: the root plus/minus object, the incrementer,
+ * and the decrementer.
+ * 
+ * The root holds the
+ * webkeys for both the incrementer and the decrementer, and also has the method
+ * that allows the webkey holder (Alice) to read
+ * the current count. Alice gives the incrementer webkey to Bob,
+ * and the decrementer webkey to Carol.
+ * 
+ * Because of the nature of the fine grain access via the webkeys, Bob
+ * can only increment, he can neither decrement nor see the current count.
+ * Carol can similarly only decrement.
+ * */
 module.exports = function PlusMinus() {
     "use strict";
-    function makeCounterPair(context) {
+    function makeRoot(context) {
         var mem = context.state;
         if (!("incr" in mem)) {
             mem.counter = context.make("shared");
@@ -22,9 +49,7 @@ module.exports = function PlusMinus() {
             init: function(sharedCounter) {
                 if (!mem.counter) {mem.counter = sharedCounter;}                
             },
-            increment: function() {
-                return ++mem.counter.state.count;
-            }
+            increment: function() {++mem.counter.state.count;}
         });
     }
     function makeMinus(context) {        
@@ -33,14 +58,12 @@ module.exports = function PlusMinus() {
             init: function(sharedCounter) {
                 if (!mem.counter) {mem.counter = sharedCounter;}                
             },
-            increment: function() { 
-                return --mem.counter.state.count;
-            }
+            increment: function() {--mem.counter.state.count;}
         });
     }
 
     return Object.freeze({
-        makeCounterPair: makeCounterPair,
+        makeRoot: makeRoot,
         makePlus: makePlus,
         makeMinus: makeMinus
     });
--- apps/sequencer/server/main.js
+++ apps/sequencer/server/main.js
@@ -1,5 +1,13 @@
 /*global console */
 
+/**
+ * Sequencer is used by the mocha tests to exercise the sequentiality
+ * of multiple operations on a single object. Each append concatenates
+ * some text (usually a single character) to the content string. After
+ * a series of such operations one can retrieve the content to assess
+ * whether the operations were performed and checkpointed in correct FIFO
+ * order.
+ * */
 module.exports = function Sequencer(context) {
     "use strict";
     context.state.x = context.state.x || "";
